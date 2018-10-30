8f10b2fc9c6ef62f3369bf7cb3abcfa9c4894638
Graeme Congreve <junkmailgmc@gmail.com>
2018-10-30 15:52:28 +0000

    finished MVP   and one extension

--- console.rb
+++ console.rb
@@ -1,3 +1,13 @@
 require_relative('models/property_tracker')
 
-property1 = PropertyTracker.new({'address' => '1 Main St.', 'value' => '100000', 'year_built' => '1955', 'buy_let' => 'buy'})
+property1 = PropertyTracker.new({'address' => '1 Main St.', 'value' => '100000', 'year_built' => '1950', 'buy_let' => 'buy'})
+
+property1.save
+
+property1.year_built = "2001"
+
+property1.update
+#property1.delete_all
+property1.save
+
+property1.find('15')
--- models/property_tracker.rb
+++ models/property_tracker.rb
@@ -3,6 +3,7 @@ require('pg')
 class PropertyTracker
 
   attr_accessor :address, :value, :year_built, :buy_let
+  attr_reader :id
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
@@ -12,4 +13,54 @@ class PropertyTracker
     @buy_let = options['buy_let']
   end
 
-end
+
+
+  def save()
+    db = PG.connect({dbname: 'property_database', host: 'localhost'})
+    sql = "INSERT INTO property_tracker
+    (address,
+      value,
+      year_built,
+      buy_let)
+      VALUES
+      ($1, $2, $3, $4) RETURNING *;"
+
+      values = [@address, @value, @year_built, @buy_let]
+      db.prepare("save", sql)
+      array_of_hashes = db.exec_prepared("save", values)
+      @id = array_of_hashes[0]['id'].to_i
+      db.close()
+    end
+
+    def update()
+      db = PG.connect({dbname: 'property_database', host: 'localhost'})
+      sql = "UPDATE property_tracker
+      SET
+      (address,
+        value,
+        year_built,
+        buy_let) = ($1, $2, $3, $4) WHERE id = $5"
+        values = [@address, @value, @year_built, @buy_let, @id]
+        db.prepare("update", sql)
+        db.exec_prepared("update", values)
+        db.close()
+      end
+
+      def delete_all()
+        db = PG.connect({dbname: 'property_database', host: 'localhost'})
+        sql = "DELETE FROM property_tracker;"
+        db.prepare("delete_all", sql)
+        db.exec_prepared("delete_all")
+        db.close()
+      end
+
+      def find(id_number)
+        db = PG.connect({dbname: 'property_database', host: 'localhost'})
+        sql = "SELECT * FROM property_tracker WHERE id = #{id_number};"
+        db.prepare("find", sql)
+        house_array = db.exec_prepared("find")
+        db.close()
+        puts house_array[0]['address']
+      end
+
+    end
